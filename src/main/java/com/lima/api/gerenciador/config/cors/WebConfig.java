383be8bd944375c97ad2679c7b46f28c3a6324ac
Anderson Lima <anderson.lima-external@hamburgsud.com>
2024-07-05 22:30:36 -0300

    implementando documentação swagger openAPI

--- src/main/java/com/lima/api/gerenciador/config/cors/WebConfig.java
+++ src/main/java/com/lima/api/gerenciador/config/cors/WebConfig.java
@@ -1,4 +1,4 @@
-package com.lima.api.gerenciador.config;
+package com.lima.api.gerenciador.config.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- src/main/java/com/lima/api/gerenciador/controller/PessoaController.java
+++ src/main/java/com/lima/api/gerenciador/controller/PessoaController.java
@@ -6,13 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lima.api.gerenciador.dto.PessoaDTO;
 import com.lima.api.gerenciador.exception.ErrorResponse;
@@ -21,7 +21,7 @@ import com.lima.api.gerenciador.service.PessoaService;
 
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class PessoaController {
 
