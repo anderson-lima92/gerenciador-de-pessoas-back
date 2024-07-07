458d1d9c18404b77a2e361a103917edf6a44ba54
Anderson Lima <anderson.lima-external@hamburgsud.com>
2024-07-07 13:11:32 -0300

    adicionando setter para teste unitario

--- src/main/java/com/lima/api/gerenciador/model/user/User.java
+++ src/main/java/com/lima/api/gerenciador/model/user/User.java
@@ -16,10 +16,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "users")
 @Entity(name = "users")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
